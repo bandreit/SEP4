f90e684630a6efe6dfbf25e3b8b50b2568ccaa68
Bogdan Novac <293134@via.dk>
2021-04-25 13:20:31 +0200

    added settings_activity

---
+++ app/app/src/main/java/com/warehouse/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.warehouse;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        System.out.println();
+    }
+}
