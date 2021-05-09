925a2ddfcae3dad9167c59cd5a4744a338626ab8
Andrei Bostan <abostan.ir@gmail.com>
2021-05-09 15:19:16 +0200

    added downlink

--- IoT/Semester4/Semester4/LoRaWANUplinkHandler.c
+++ IoT/Semester4/Semester4/LoRaWANUplinkHandler.c
@@ -134,7 +134,7 @@
 	 
 	 xTaskCreate(
 	 lora_uplink_handler_task,
-	, "LRHand"  // A name just for humans
+	, "LRHandUplink"  // A name just for humans
 	, configMINIMAL_STACK_SIZE+200  // This stack size can be checked & adjusted by reading the Stack Highwater
 	, NULL
 	, lora_handler_task_priority  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
