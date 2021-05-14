38c0a70c4d9b682a1da033f189323483ba9d2d24
WorldFam <mirekas123@gmail.com>
2021-05-14 12:48:05 +0200

    Settings fragment initial commit

---
+++ app/src/main/java/com/warehouse/SettingsFragment.java
@@ -0,0 +1,59 @@
+package com.warehouse;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+
+public class SettingsFragment extends Fragment {
+    SwitchMaterial notificationSwitch, themeModeSwitch;
+    TextView enabledNotification, enableThemeMode;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        notificationSwitch = view.findViewById (R.id.notificationSwitch);
+        themeModeSwitch = view.findViewById (R.id.themeModeSwitch);
+
+        enabledNotification = view.findViewById (R.id.NotificationsToggle);
+        enableThemeMode = view.findViewById (R.id.ThemeModeToggle);
+
+        notificationSwitch.setChecked (true);
+
+       notificationSwitch.setOnCheckedChangeListener (new CompoundButton.OnCheckedChangeListener ( ) {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               if(isChecked){
+                   enabledNotification.setText (R.string.title_enabled);
+               }
+               else {
+                   enabledNotification.setText (R.string.title_disabled);
+               }
+           }
+       });
+
+       themeModeSwitch.setOnCheckedChangeListener (new CompoundButton.OnCheckedChangeListener ( ) {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    enableThemeMode.setText (R.string.title_enabled);
+                }
+                else {
+                    enableThemeMode.setText (R.string.title_disabled);
+                }
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/warehouse/formaters/DayFormatter.java
@@ -0,0 +1,28 @@
+package com.warehouse.formaters;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+public class DayFormatter extends ValueFormatter {
+    @Override
+    public String getFormattedValue(float value) {
+        switch (Math.round(value)) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            default:
+                return String.valueOf(value);
+        }
+    }
+}
