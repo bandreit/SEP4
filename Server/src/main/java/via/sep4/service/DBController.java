3ec25ab7f4b691d5b6f7b1cd2063e3febd23b159
Sabina <elenasabina@ymail.com>
2021-05-25 12:35:30 +0200

    started cloud

--- Server/src/main/java/via/sep4/service/DBController.java
+++ Server/src/main/java/via/sep4/service/DBController.java
@@ -3,7 +3,7 @@ package via.sep4.service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import via.sep4.Parameter;
+import via.sep4.model.Parameter;
 import via.sep4.persistance.DatabaseAdaptor;
 import via.sep4.persistance.DatabasePersistence;
 
