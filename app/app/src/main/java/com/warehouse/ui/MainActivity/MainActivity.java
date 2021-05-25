aceab22f622956910284b24da8fc32b7aab38d7a
Bogdan Novac <293134@via.dk>
2021-05-25 21:26:21 +0200

    Design bottom sheet layout

--- app/app/src/main/java/com/warehouse/ui/MainActivity/MainActivity.java
+++ app/app/src/main/java/com/warehouse/ui/MainActivity/MainActivity.java
@@ -8,10 +8,13 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.warehouse.R;
 import com.warehouse.ui.LoginActivity.LoginActivity;
+import com.warehouse.ui.configure.BottomSheetDialog;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         startAppActivity();
 
         checkIfSignedIn();
+
+     
     }
 
     public void checkIfSignedIn() {
