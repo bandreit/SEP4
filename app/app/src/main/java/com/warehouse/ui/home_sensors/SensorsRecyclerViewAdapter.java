46ce71db2d714b9257d4b4532a2e318885510b57
Bogdan Novac <293134@via.dk>
2021-05-25 22:01:08 +0200

    Made Bottom Sheet Dialog Work

--- app/app/src/main/java/com/warehouse/ui/home_sensors/SensorsRecyclerViewAdapter.java
+++ app/app/src/main/java/com/warehouse/ui/home_sensors/SensorsRecyclerViewAdapter.java
@@ -3,21 +3,31 @@ package com.warehouse.ui.home_sensors;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.warehouse.R;
 import com.warehouse.data.Room.Sensor;
+import com.warehouse.ui.configure.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SensorsRecyclerViewAdapter extends RecyclerView.Adapter<SensorsRecyclerViewAdapter.ViewHolder> {
     List<Sensor> sensors = new ArrayList<>();
+    FragmentManager fragmentManager;
 
-    public SensorsRecyclerViewAdapter() {
+    View view;
+
+
+    public SensorsRecyclerViewAdapter(FragmentManager fragmentManager) {
+        this.fragmentManager=fragmentManager;
+
     }
 
     public void setSensors(List<Sensor> sensors) {
@@ -29,7 +39,7 @@ public class SensorsRecyclerViewAdapter extends RecyclerView.Adapter<SensorsRecy
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.home_sensor_item, parent, false);
+         view = inflater.inflate(R.layout.home_sensor_item, parent, false);
 
         return new ViewHolder(view);
     }
@@ -37,6 +47,15 @@ public class SensorsRecyclerViewAdapter extends RecyclerView.Adapter<SensorsRecy
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.sensorName.setText(sensors.get(position).getName());
+        Button configureButton= view.findViewById(R.id.configureBtn);
+        configureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog= new BottomSheetDialog();
+
+                bottomSheetDialog.show(  fragmentManager,"Bla bla");
+            }
+        });
     }
 
     @Override
@@ -51,6 +70,10 @@ public class SensorsRecyclerViewAdapter extends RecyclerView.Adapter<SensorsRecy
             super(itemView);
 
             sensorName = itemView.findViewById(R.id.sensorName);
+
+
+
+
         }
     }
 }
