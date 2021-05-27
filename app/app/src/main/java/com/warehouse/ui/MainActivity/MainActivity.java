2299aa92a7ba8c71808ceff228858125668720d3
Bogdan Novac <293134@via.dk>
2021-05-27 10:05:40 +0200

    Made Bottom Sheet Dialog Work

--- app/app/src/main/java/com/warehouse/ui/MainActivity/MainActivity.java
+++ app/app/src/main/java/com/warehouse/ui/MainActivity/MainActivity.java
@@ -8,13 +8,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.warehouse.R;
 import com.warehouse.ui.LoginActivity.LoginActivity;
-import com.warehouse.ui.configure.BottomSheetDialog;
+
 
 
 public class MainActivity extends AppCompatActivity {
