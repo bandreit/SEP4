26259e2cb68a29f93dc9000ee1a8ad13a66f6aa7
Sabina <elenasabina@ymail.com>
2021-06-02 21:53:12 +0200

    changed database instance

--- Server/src/main/java/via/sep4/ServiceApiApplication.java
+++ Server/src/main/java/via/sep4/ServiceApiApplication.java
@@ -22,19 +22,7 @@ public class ServiceApiApplication {
      */
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ServiceApiApplication.class);
-        Map<String, Object> properties = new HashMap<String, Object>();
-        properties.put("spring.datasource.driver-class-name", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        properties.put("spring.datasource.url", "jdbc:sqlserver://ec2-3-124-3-27.eu-central-1.compute.amazonaws.com:10001;DatabaseName=SEP4");
-        properties.put("spring.datasource.username", "Admin");
-        properties.put("spring.datasource.password", "12345678");
-        properties.put("spring.jpa.hibernate.ddl-auto", "create-drop");
-        properties.put("spring.jpa.properties.hibernate.default_schema", "dbo");
-        properties.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
-        properties.put("spring.jpa.show-sql", "true");
-        properties.put("spring.jpa.properties.hibernate.format_sql", "true");
-        properties.put("spring.jpa.properties.hibernate.enable_lazy_load_no_trans", "true");
-        properties.put("server.port", "10002");
-        app.setDefaultProperties(properties);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "10001"));
         app.run(args);
 
         System.out.println("Service started.");
