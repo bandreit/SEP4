ad8cde04318b03cecc7ad37c56db98f22d0dc24d
Andrei Bostan <abostan.ir@gmail.com>
2021-06-04 12:00:09 +0200

    final gateway changes

--- GatewayJava/src/main/java/Main.java
+++ GatewayJava/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import mediator.ClientHandler;
 import network.NetworkPackage;
 import sensor.SensorHistory;
 import service.WebsocketClient;
@@ -7,6 +8,7 @@ import mediator.ConnectionManager;
 import network.NetworkType;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -28,8 +30,9 @@ public class Main {
      */
 
     public static void main(String[] args) throws IOException {
-        Gson gson = new Gson();
         WebsocketClient websocketClient = new WebsocketClient("wss://iotnet.cibicom.dk/app?token=vnoTuwAAABFpb3RuZXQuY2liaWNvbS5kaxOt1kIOlKdP0z7zj8xIG_I=");
+
+        Gson gson = new Gson();
 
         ClientHandler clientHandler = new ClientHandler(websocketClient);
         Thread t = new Thread(clientHandler);
@@ -38,37 +41,42 @@ public class Main {
 
         while (true) {
             try {
-                ArrayList<SensorHistory> sensorArrayList = new ArrayList<>();
+//                Only Needed To Mock Values
+//                sendRandomValues(gson);
 
-                double decVal = getRandomValue(rnd, 1, 1251, 2);
-                int room = getRandomNumber(rooms.length - 1);
-                long timestamp = System.currentTimeMillis();
-
-                SensorHistory co2Sensor = new SensorHistory(CO2_SENSORS[room], timestamp, decVal);
-                System.out.println(co2Sensor.toString());
-                sensorArrayList.add(co2Sensor);
-
-                decVal = getRandomValue(rnd, 1, 500, 2);
-                SensorHistory humSensor = new SensorHistory(HUM_SENSORS[room], timestamp, decVal);
-                System.out.println(humSensor.toString());
-                sensorArrayList.add(humSensor);
-
-                decVal = getRandomValue(rnd, 30, 40, 2);
-                SensorHistory temperatureSensor = new SensorHistory(TEMP_SENSORS[room], timestamp, decVal);
-                System.out.println(temperatureSensor.toString());
-                sensorArrayList.add(temperatureSensor);
-
-                ConnectionHandler handler = ConnectionManager.getInstance();
-                NetworkPackage networkPackage = new NetworkPackage(NetworkType.SensorList, sensorArrayList);
-                String gsonToServer = gson.toJson(networkPackage);
-                handler.sendToServer(gsonToServer);
-                sensorArrayList.clear();
-
-                Thread.sleep(30000);
-            } catch (InterruptedException | IOException e) {
+                Thread.sleep(300000);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void sendRandomValues(Gson gson) throws IOException {
+        ArrayList<SensorHistory> sensorArrayList = new ArrayList<>();
+
+        double decVal = getRandomValue(rnd, 1, 1251, 2);
+        int room = getRandomNumber(rooms.length - 1);
+        long timestamp = getRandomTimestamp();
+
+        SensorHistory co2Sensor = new SensorHistory(CO2_SENSORS[room], timestamp, decVal);
+        System.out.println(co2Sensor.toString());
+        sensorArrayList.add(co2Sensor);
+
+        decVal = getRandomValue(rnd, 1, 500, 2);
+        SensorHistory humSensor = new SensorHistory(HUM_SENSORS[room], timestamp, decVal);
+        System.out.println(humSensor.toString());
+        sensorArrayList.add(humSensor);
+
+        decVal = getRandomValue(rnd, 30, 40, 2);
+        SensorHistory temperatureSensor = new SensorHistory(TEMP_SENSORS[room], timestamp, decVal);
+        System.out.println(temperatureSensor.toString());
+        sensorArrayList.add(temperatureSensor);
+
+        ConnectionHandler handler = ConnectionManager.getInstance();
+        NetworkPackage networkPackage = new NetworkPackage(NetworkType.SensorList, sensorArrayList);
+        String gsonToServer = gson.toJson(networkPackage);
+        handler.sendToServer(gsonToServer);
+        sensorArrayList.clear();
     }
 
     /**
@@ -96,5 +104,12 @@ public class Main {
 
     private static int getRandomNumber(int max) {
         return (int) Math.floor(Math.random() * (max + 1));
+    }
+
+    private static long getRandomTimestamp() {
+        long offset = Timestamp.valueOf("2021-04-01 00:00:00").getTime();
+        long end = Timestamp.valueOf("2021-06-04 00:00:00").getTime();
+        long diff = end - offset + 1;
+        return new Timestamp(offset + (long) (Math.random() * diff)).getTime();
     }
 }
